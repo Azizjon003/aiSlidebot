9a241c9d9b0a23531b68be6705494b07f267248b
Azizjon <aliqulovazizjon68@gmail.com>
2024-03-04 09:20:19 +0500

    Refactor scene.hears("Balans") in control.ts and scene.action(/^balance/) in balans.ts

--- src/scenes/balans.ts
+++ src/scenes/balans.ts
@@ -5,8 +5,37 @@ import { keyboards } from "../utils/keyboards";
 const scene = new Scenes.BaseScene("balans");
 
 scene.hears("/start", (ctx: any) => {
-  ctx.reply("Bosh sahifadasiz");
   ctx.scene.enter("start");
 });
 
+scene.action(
+  /^balance:([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$/,
+  async (ctx: any) => {
+    const user_id = ctx.from.id;
+    const user = await prisma.user.findFirst({
+      where: {
+        telegram_id: String(user_id),
+      },
+    });
+
+    ctx.answerCbQuery();
+
+    if (!user) return ctx.reply("Bu foydalanuchi mavjud emas");
+
+    const wallet = await prisma.wallet.findFirst({
+      where: {
+        user_id: user.id,
+      },
+    });
+
+    if (!wallet) {
+      return ctx.reply("Bu foydalanuchi mavjud emas");
+    }
+
+    const text = `Balansingiz: ${wallet.balance}.\nBalansni to'ldirish uchun summani kiriting:`;
+    ctx.reply(text);
+
+    ctx.scene.enter("start");
+  }
+);
 export default scene;
--- src/scenes/control.ts
+++ src/scenes/control.ts
@@ -1,16 +1,15 @@
 import { Scenes } from "telegraf";
 import enabled from "../utils/enabled";
 import prisma from "../../prisma/prisma";
-import { keyboards } from "../utils/keyboards";
+import { createInlineKeyboard, keyboards } from "../utils/keyboards";
 import { getBalance } from "../utils/isBalance";
 const scene = new Scenes.BaseScene("control");
 
 scene.hears("/start", (ctx: any) => {
-  ctx.reply("Bosh sahifadasiz");
   ctx.scene.enter("start");
 });
 
-scene.hears("Balans", async (ctx) => {
+scene.hears("Balans", async (ctx: any) => {
   const user_id = ctx.from.id;
   const user = await prisma.user.findFirst({
     where: {
@@ -20,6 +19,33 @@ scene.hears("Balans", async (ctx) => {
 
   if (!user) return ctx.reply("Bu foydalanuchi mavjud emas");
 
-  const wallet = getBalance(user.id);
+  const wallet = await getBalance(user.id);
+  let priceSlide = await prisma.plansSlides.findFirst({
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  if (!priceSlide) {
+    priceSlide = await prisma.plansSlides.create({
+      data: {
+        price: 2000,
+      },
+    });
+  }
+
+  const text = `Balansingiz: ${
+    wallet.balance
+  }\nSiz olishingiz mumkin bo'lgan slidelar soni: ${Math.floor(
+    wallet.balance / Number(priceSlide?.price)
+  )}`;
+  const inlineKeyboard = [
+    {
+      text: "Balansni to'ldirish",
+      callbackData: `balance:${user?.id}`,
+    },
+  ];
+  ctx.reply(text, createInlineKeyboard(inlineKeyboard));
+
+  ctx.scene.enter("balans");
 });
 export default scene;
