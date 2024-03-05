f31c4a7eb31457c009424fd73d138a97287e9a2a
Azizjon <aliqulovazizjon68@gmail.com>
2024-03-05 16:37:36 +0500

    Refactor createPlans function to generate plans for power point presentations

--- src/services/createPlansUseOpenAi.ts
+++ src/services/createPlansUseOpenAi.ts
@@ -12,17 +12,34 @@ async function createPlans(name: string, pages: number) {
       { role: "user", content: name },
       {
         role: "system",
-        content: `Sen menga berilgan mavzu bo'yicha professor tuzib bera oladigan darajada power point uchun reja tuzib ber.Menga ${pages} ta rejali qilib tuzib ber.Bunda rejalar aniq va bitta gapdan iborat bo'lsin.Beriladigan matnda faqat rejalar bo'lsin ortiqcha gaplardan foydalanish  taqiqlanadi.`,
+        content: `Make a plan for power point as much as the professor can make on the given topic. Make a plan for ${pages} for me. Make sure the plans are clear and consist of one sentence. use is prohibited. Plans should be in Uzbek`,
+        // content: `Sen menga berilgan mavzu bo'yicha professor tuzib bera oladigan darajada power point uchun reja tuzib ber.Menga ${pages} ta rejali qilib tuzib ber.Bunda rejalar aniq va bitta gapdan iborat bo'lsin.Beriladigan matnda faqat rejalar bo'lsin ortiqcha gaplardan foydalanish  taqiqlanadi.`,
       },
     ],
     model: "gpt-3.5-turbo-0125",
     max_tokens: 4096,
   });
 
+  console.log(chatCompletion.choices);
+
   let text = chatCompletion.choices[0].message.content;
 
   const reja = rejalarniAjratibOlish(String(text));
-  console.log(reja);
+  return reja;
 }
 
-createPlans("Odam salomatligi ", 15);
+createPlans("O'zbekiston Iqtisodiyoti", 10);
+export async function createPlansDescription(description: string) {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      { role: "user", content: description },
+      {
+        role: "system",
+        content: ``,
+        // content: `Sen menga berilgan mavzu bo'yicha professor tuzib bera oladigan darajada power point uchun reja tuzib ber.Menga ${pages} ta rejali qilib tuzib ber.Bunda rejalar aniq va bitta gapdan iborat bo'lsin.Beriladigan matnda faqat rejalar bo'lsin ortiqcha gaplardan foydalanish  taqiqlanadi.`,
+      },
+    ],
+    model: "gpt-3.5-turbo-0125",
+    max_tokens: 4096,
+  });
+}
