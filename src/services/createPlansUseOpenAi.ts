abe25727aa32a398bdabee2ebfe1aa19d7977fca
azizjon003 <aliqulovazizjon68@gmail.com>
2024-08-20 13:22:48 +0500

    gpt model updated to gpt4 mini

--- src/services/createPlansUseOpenAi.ts
+++ src/services/createPlansUseOpenAi.ts
@@ -10,61 +10,6 @@ const openai = new OpenAI({
 
 const djJson = require("dirty-json");
 const xss = require("xss");
-// export let createPlans = async (name: string, pages: number) => {
-//   const queryJson = {
-//     input_text: `Create ${pages} layout for topic. Create 20 to 30 words for each plan. ${name}. Each plan must have {{uz}}, {{eng}} in Uzbek and English. The end result should look like this. List of discussion questions. Return as JSON.`,
-//     output_format: "json",
-//     json_structure: {
-//       slides: {
-//         plans: [
-//           {
-//             uzTitle: "{{uzTitle}}",
-//             enTitle: "{{enTitle}}",
-//           },
-//         ],
-//       },
-//     },
-//   };
-
-//   // try {
-//   const response = await axios.post(
-//     "https://api.openai.com/v1/chat/completions",
-//     {
-//       model: "gpt-3.5-turbo-0125",
-//       messages: [
-//         {
-//           role: "user",
-//           content: JSON.stringify(queryJson),
-//         },
-//       ],
-//       max_tokens: 1024,
-//       response_format: {
-//         type: "json_object",
-//       },
-//     },
-//     {
-//       headers: {
-//         Authorization: `Bearer ${key}`,
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
-
-//   // console.log(response.data.choices[0].message.content);
-//   const content = response.data.choices[0].message.content || "";
-//   let plans = JSON.parse(content).slides.plans;
-
-//   let plansText = plans.map((plan: any) =>
-//     `${plan.uzTitle} && ${plan.enTitle}`.replace(/\d+/g, "")
-//   );
-
-//   return plansText;
-//   // } catch (error: any) {
-//   //   console.error("Error during API call:", error.data);
-//   //   // Xato bilan bog'liq qo'shimcha ishlov berish
-//   //   throw error; // Yoki xato haqida ma'lumot berish
-//   // }
-// };
 
 export let createPlans = async (name: string, pages: number) => {
   const queryJson = {
@@ -133,41 +78,6 @@ export let createPlans = async (name: string, pages: number) => {
 
   return plansText;
 };
-
-// createPlans("Qon tomir kasallilari", 10);
-
-// export async function createPlansDescription(description: string, datas: any) {
-//   const chatCompletion = await openai.chat.completions.create({
-//     messages: [
-//       { role: "user", content: description },
-//       ...datas,
-//       {
-//         role: "system",
-//         content: `Please provide content for a PowerPoint slide on the topic "${description}", written in a concise and scientific manner in native Uzbek language, tailored for educational purposes. Ensure the content adheres to this structure:
-
-//         - Key Facts: [2-3 important, scientifically-backed facts about the topic]
-//         - Conclusion: [A brief conclusion summarizing the topic's impact or significance]
-
-//         This content must be prepared to enhance the presentation's clarity and educational value, aiming to provide clear, useful, and scientifically validated information on the subject to the audience in Uzbek.
-//         `,
-//         // content:
-//         //   "Give the information provided by the role of the teacher on the given topic. Do not let it be known that this information was given by the teacher. Write down the information you wrote down. The information should be in Uzbek. Who wrote the information not be known at all",
-//         // content:
-//         // "Write down the information provided by a 20-year-old teacher on the given topic and give more information. Do not use unnecessary texts in this information. Do not mix unnecessary texts at all. Be in Uzbek. Texts",
-//         // content: `Sen menga berilgan mavzu bo'yicha professor tuzib bera oladigan darajada power point uchun reja tuzib ber.Menga ${pages} ta rejali qilib tuzib ber.Bunda rejalar aniq va bitta gapdan iborat bo'lsin.Beriladigan matnda faqat rejalar bo'lsin ortiqcha gaplardan foydalanish  taqiqlanadi.`,
-//       },
-//       {
-//         role: "user",
-//         content: "",
-//       },
-//     ],
-
-//     model: "gpt-4-1106-preview",
-//     max_tokens: 4096,
-//   });
-
-//   return chatCompletion.choices[0].message.content;
-// }
 
 export let createPlansDescription = async (name: string) => {
   const queryJson = {
@@ -251,75 +161,6 @@ export let createPlansDescription = async (name: string) => {
   };
 };
 
-// export let createPlansDescription = async (name: string) => {
-//   const queryJson = {
-//     // input_text: `Provide the necessary information on the topic. Create 50 to 60 words for your topic. ${name}. {{uz}} for each topic should be in Uzbek language. The end result should be like this. List of discussion questions. Return as JSON based on the given structure. Please do not deviate from the given structure. Every information should be in Uzbek language. In Title, the name of the topic for the part of the slide should be in Uzbek. And in UzContent, there should be the necessary information for this topic. The return value should be in JSON format`,
-//     input_text: `Provide the necessary information on the topic. Create 30 to 50 words for your topic. ${name}. {{uz}} for each topic should be in Uzbek language. The end result should be like this. List of discussion questions. Return as JSON based on the given structure. Please do not deviate from the given structure. All information must be in Uzbek. In the title, the name of the topic for the slide section should be in Uzbek. UzContent should have the necessary information on this topic. The return value must be in JSON format.finish_reason should not exceed 4096 tokens.`,
-//     output_format: "json",
-//     json_structure: {
-//       slide: {
-//         name: "{{name}}",
-//         content: [
-//           {
-//             title: "{{title}}",
-//             uzContent: "{{uzContent}}",
-//           },
-//           {
-//             title: "{{title}}",
-//             uzContent: "{{uzContent}}",
-//           },
-//           {
-//             title: "{{title}}",
-//             uzContent: "{{uzContent}}",
-//           },
-//           {
-//             title: "{{title}}",
-//             uzContent: "{{uzContent}}",
-//           },
-//         ],
-//       },
-//     },
-//   };
-
-//   // try {
-//   const response = await axios.post(
-//     "https://api.openai.com/v1/chat/completions",
-//     {
-//       model: "gpt-4-turbo-preview", // Specify the model here
-//       messages: [
-//         { role: "user", content: name },
-//         {
-//           role: "system",
-//           content: JSON.stringify(queryJson),
-//         },
-//       ],
-//       max_tokens: 800,
-//       response_format: {
-//         type: "json_object",
-//       },
-//     },
-//     {
-//       headers: {
-//         Authorization: `Bearer ${key}`, // Replace with your actual API key
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
-
-//   const description = JSON.parse(response.data.choices[0].message.content ?? "")
-//     .slide.content;
-
-//   console.log(description);
-//   return {
-//     name: name,
-//     content: description,
-//   };
-//   // } catch (error) {
-//   //   console.error("Error during API call:", error.response?.data || error.message);
-//   //   throw error; // Or handle the error as needed
-//   // }
-// };
-
 export let createPlansLanguage = async (
   name: string,
   pages: number,
@@ -329,28 +170,14 @@ export let createPlansLanguage = async (
   model: modelLang = modelLang.gpt3
 ) => {
   let models = {
-    "gpt-3": "gpt-3.5-turbo-0125",
+    "gpt-3": "gpt-4o-mini",
     "gpt-4": "gpt-4o-2024-05-13",
   };
   console.log(models["gpt-3"], pages);
   if (lang == "eng") {
     lang = "english";
   }
-  // const queryJson = {
-  //   input_text: `Create a ${pages} layout for the theme. Create 20 to 30 words for each plan. ${name}. Each plan must have ${language} and {{${lang}}}, {{eng}} in English. The end result should be like this. List of discussion questions. Return as JSON. Do not contain data that violates the JSON format. Plans should only contain words.`,
-  //   // input_text: `Create ${pages} layout for topic. Create 20 to 30 words for each plan. ${name}. Each plan must have {${lang}}, {eng} in ${language} and English. The end result should look like this. List of discussion questions. Return as JSON.`,
-  //   output_format: "json",
-  //   json_structure: {
-  //     slides: {
-  //       plans: [
-  //         {
-  //           [lang]: `{{${lang}}}`,
-  //           eng: "{{eng}}",
-  //         },
-  //       ],
-  //     },
-  //   },
-  // };
+
   let queryJson = {
     input_text: `Create a layout with ${pages} pages for the theme '${name}'.Make appropriate professional plans for the given topic. Each page should have plans with descriptions of 30 to 40 words in both ${language} and English. The plans should be structured in a way that each contains a version in ${language} and a version in English. Ensure that the data is formatted correctly and that the plans contain only textual information.
     Pay attention to the language of presentation - ${language}.
@@ -384,6 +211,7 @@ export let createPlansLanguage = async (
     // model: "gpt-4-turbo-preview",
     model: models[model],
     // model: "gpt-3.5-turbo-0125",
+
     max_tokens: pagesCount < 6 ? 1200 : pagesCount < 12 ? 1600 : 1800,
     response_format: {
       type: "json_object",
@@ -391,7 +219,7 @@ export let createPlansLanguage = async (
   });
   console.log(chatCompletion.choices[0].message.content);
   const content = chatCompletion.choices[0].message.content || ""; // Handle null case
-  // const plans = parseTitles(content);
+
   let plans;
   try {
     plans = JSON.parse(content).slides.plans;
@@ -492,7 +320,7 @@ export let createPlansDescriptionLanguage = async (
   model: modelLang = modelLang.gpt3
 ) => {
   let models = {
-    "gpt-3": "gpt-3.5-turbo-0125",
+    "gpt-3": "gpt-4o-mini",
     "gpt-4": "gpt-4o-2024-05-13",
   };
 
@@ -545,6 +373,7 @@ export let createPlansDescriptionLanguage = async (
       },
     },
   };
+  console.log(models[model]);
 
   const chatCompletion = await openai.chat.completions.create({
     messages: [
@@ -663,4 +492,4 @@ const test = async () => {
   }
 };
 
-// test();
+test();
