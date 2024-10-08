2802944ffaf022e26cd87b61b091ae581f377946
azizjon003 <aliqulovazizjon68@gmail.com>
2024-10-08 15:32:57 +0500

    feat: Add feedback functionality to handle user feedback
    Refactor the feedback.ts file to handle user feedback in the "message" event. This includes capturing the user's ID, name, and feedback text, and sending it to a specific channel for review. Additionally, add error handling to handle any potential errors during the process.
    
    This commit enhances user engagement by allowing users to provide feedback to the bot and helps the bot's developers gather valuable insights and suggestions from users.

--- src/scenes/feadback.ts
+++ src/scenes/feadback.ts
@@ -8,25 +8,29 @@ scene.hears("/start", async (ctx: any) => {
 });
 
 scene.on("message", async (ctx: any) => {
-  const user_id = String(ctx.from.id);
-  const user = await prisma.user.findFirst({
-    where: {
-      telegram_id: user_id,
-    },
-  });
+  try {
+    const user_id = String(ctx.from.id);
+    const user = await prisma.user.findFirst({
+      where: {
+        telegram_id: user_id,
+      },
+    });
 
-  if (!user) {
-    ctx.reply("You are not registered. Please /start the bot first.");
-    return;
+    if (!user) {
+      ctx.reply("You are not registered. Please /start the bot first.");
+      return;
+    }
+    const txt = ctx.message.text;
+    await ctx.telegram.sendMessage(
+      -4594205360,
+      xss(`foydalanuvchi: ${user.name} \n userId: ${user_id} \n fikr: ${txt}`)
+    );
+
+    await ctx.reply("Rahmat fikringiz uchun 🙏 )");
+
+    return await ctx.scene.enter("start");
+  } catch (error) {
+    console.log(error, "xatolik");
   }
-  const txt = ctx.message.text;
-  ctx.telegram.sendMessage(
-    -4594205360,
-    xss(`foydalanuvchi: ${user.name} \n userId: ${user_id} \n fikr: ${txt}`)
-  );
-
-  ctx.reply("Rahmat fikringiz uchun 🙏 )");
-
-  return await ctx.scene.enter("start");
 });
 export default scene;
