dd3e444aec84f87202b7cfe8247319d650e58181
azizjon003 <aliqulovazizjon68@gmail.com>
2024-10-16 19:08:47 +0500

    refactor: Update createPlansUseOpenAi.ts to use gpt-4 model
    Update createPlansUseOpenAi.ts to use the gpt-4 model instead of the previous model. This change ensures that the code is using the most up-to-date and appropriate model for generating plans.

--- src/scenes/message.ts
+++ src/scenes/message.ts
@@ -1,3 +1,4 @@
+import { User } from "@prisma/client";
 import { Scenes } from "telegraf";
 import xss from "xss";
 import prisma from "../../prisma/prisma";
@@ -9,13 +10,22 @@ scene.hears("/start", async (ctx: any) => {
 
 scene.on("message", async (ctx: any) => {
   const users = await prisma.user.findMany();
+
+  messageFunction(users, ctx);
+  ctx.reply(
+    "Xabar barcha foydalanuvchilarga yuborildi.\n Qanday xizmatlar bor menga admin :)"
+  );
+  return await ctx.scene.enter("start");
+});
+
+const messageFunction = async (users: User[], ctx: any) => {
   for (let user of users) {
     const userNameClean = xss(user.username ?? "Anonymous");
     try {
       await ctx.telegram.sendMessage(user.telegram_id, ctx.message.text, {
         parse_mode: "HTML",
       });
-      await delay(1000);
+      await delay(500);
     } catch (error) {
       console.log(error);
       ctx.reply(
@@ -26,11 +36,6 @@ scene.on("message", async (ctx: any) => {
       );
     }
   }
-  ctx.reply(
-    "Xabar barcha foydalanuvchilarga yuborildi.\n Qanday xizmatlar bor menga admin :)"
-  );
-  return await ctx.scene.enter("start");
-});
-
+};
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 export default scene;
